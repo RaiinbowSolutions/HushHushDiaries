544c8a523f96ec3ff5e56a531d57aa3bf2b48423
Dennis Thiesen <dennis.thiesen.1990@gmail.com>
2023-08-20 04:57:17 +0200

    configured database connection

---
+++ api/src/utilities/database.ts
@@ -0,0 +1,31 @@
+import { Kysely } from "kysely";
+import { PlanetScaleDialect } from "kysely-planetscale";
+import { BlogTable } from "../models/blog.model";
+import { CategoryTable } from "../models/category.model";
+import { CommentTable } from "../models/comment.model";
+import { LikeTable } from "../models/like.model";
+import { MessageTable } from "../models/message.model";
+import { PermissionTable } from "../models/permission.model";
+import { RequestTable } from "../models/request.model";
+import { UserCredentialTable, UserDetailTable, UserOptionTable, UserPermissionTable, UserTable } from "../models/user.model";
+
+interface DatabaseSchema {
+    blogs: BlogTable,
+    categories: CategoryTable,
+    comments: CommentTable,
+    likes: LikeTable,
+    messages: MessageTable,
+    permisions: PermissionTable,
+    requests: RequestTable,
+    users: UserTable,
+    user_credentials: UserCredentialTable,
+    user_details: UserDetailTable,
+    user_options: UserOptionTable,
+    user_permissions: UserPermissionTable,
+}
+
+export const Database = new Kysely<DatabaseSchema>({
+    dialect: new PlanetScaleDialect({
+      url: process.env.PLANETSCALE_DATABASE_URL
+    }),
+});
