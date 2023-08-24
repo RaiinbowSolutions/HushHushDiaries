52b0a688f66e39695a1d87bc2576a53d1c2757ce
Dennis Thiesen <dennis.thiesen.1990@gmail.com>
2023-08-24 16:52:10 +0200

    Created AuthenticatedMiddleware

--- api/src/middleware/authenticated.middleware.ts
+++ api/src/middleware/authenticated.middleware.ts
@@ -2,6 +2,7 @@ import { Middleware } from "lambda-api";
 
 export const AuthenticatedMiddleware = (): Middleware => {
     return async (request, response, next) => {
-        return next();
+        if ('authentication' in request && request.authentication.authenticated == true) return next();
+        throw new Error(); // Needs a better error definition
     }
 };
