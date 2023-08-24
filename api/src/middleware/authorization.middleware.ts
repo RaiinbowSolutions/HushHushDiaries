4e4ebb552e5b058432dbf8d0ea7ae19528924b1f
Dennis Thiesen <dennis.thiesen.1990@gmail.com>
2023-08-24 16:50:18 +0200

    Not needed for now

--- api/src/middleware/authorization.middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Middleware } from "lambda-api";
-
-export const AuthorizationMiddleware = (): Middleware => {
-    return async (request, response, next) => {
-        return next();
-    }
-};
