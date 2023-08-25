c91d1333fd9a31d6785441874b1b26766aea51f7
Dennis Thiesen <dennis.thiesen.1990@gmail.com>
2023-08-25 05:53:33 +0200

    Updated UserRoute with new refactoring

--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,9 +1,12 @@
 import { API, RegisterOptions, Request, Response } from 'lambda-api';
 import { AuthenticatedMiddleware as Authenticated } from '../middleware/authenticated.middleware';
 import { UserService } from '../services/user.service';
-import { CreateDataResponse, CreatePaginationDataResponse } from '../utilities/responses';
 import { Validation } from '../utilities/validation';
 import { Authentication } from '../middleware/authentication.middleware';
+import { ValidateMiddleware } from '../middleware/validate.middleware';
+import { Pagination } from '../utilities/pagination';
+import { Minify } from '../utilities/minify';
+import { BadRequestError, NotFoundError } from '../middleware/error.middleware';
 
 export const UserRoute = (api: API, options: RegisterOptions | undefined) => {
     const Prefix = options?.prefix || '';
@@ -18,35 +21,59 @@ export const UserRoute = (api: API, options: RegisterOptions | undefined) => {
     );
 
     api.get(Prefix + BaseURI,
+        ValidateMiddleware('query', {
+            'page': { type: 'number', required: false },
+            'limit': { type: 'number', required: false },
+        }),
         Authenticated(),
         async (request: Request, response: Response) => {
             let authentication: Authentication = request.authentication;
-            let {limit, offset} = Validation.pagination(request);
-            let total = await UserService.counts();
-            let users = await UserService.selects(offset, limit);
-            let filteredUsers = await UserService.filters.users(authentication.id, users);
-            return CreatePaginationDataResponse(request, response, filteredUsers, total);
+            let {limit, offset} = Pagination.getData(request);
+
+            try {
+                let total = await UserService.counts();
+                let users = await UserService.selects(offset, limit);
+                let filtered = await UserService.filters.users(authentication.id, users);
+    
+                let pagination = Pagination.create(request, filtered, total);
+    
+                return response.status(200).json(pagination);
+            } catch (error) {
+                
+            }
         }
     );
 
     api.get(Prefix + BaseURI + '/[id]',
+        ValidateMiddleware('params', { 'id': 'string' }),
         Authenticated(),
         async (request: Request, response: Response) => {
             let authentication: Authentication = request.authentication;
-            let id = Validation.id(request);
-            let user = await UserService.select(id);
-            let filteredUser = await UserService.filters.user(authentication.id, user);
-            return CreateDataResponse(request, response, filteredUser);
+
+            if (!Minify.validate(request.params.id as string)) throw new NotFoundError('User not found');
+            let id = Minify.decode(request.params.id as string);
+
+            try {
+                let user = await UserService.select(id);
+                let filtered = await UserService.filters.user(authentication.id, user);
+
+                return response.status(200).json(filtered);
+            } catch (error) {
+                throw new NotFoundError('User not found');
+            }
         }
     );
 
     api.get(Prefix + BaseURI + '/email/[email]',
+        ValidateMiddleware('params', { 'email': 'string' }),
         Authenticated(),
         async (request: Request, response: Response) => {
-            let email = Validation.email(request);
-            let userId = await UserService.findUserIdByEmail(email);
+            if (Validation.email(request.params.email as string)) throw new BadRequestError(`Given 'email' is not an valid email address`);
+            let id = await UserService.findUserIdByEmail(request.params.email as string);
 
-            return response.redirect(308, `/${userId}`);
+            if (id === undefined) throw new NotFoundError('User not found');
+
+            return response.redirect(308, `/${id}`);
         }
     );
 
