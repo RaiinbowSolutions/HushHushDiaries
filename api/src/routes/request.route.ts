000110149d0683b7f0dc51e98a6a1ffd66eea4e7
Christine Wiberg <chrissywhiskerz@outlook.com>
2023-08-27 22:36:11 +0200

    Created templates for last end points - Request

--- api/src/routes/request.route.ts
+++ api/src/routes/request.route.ts
@@ -61,4 +61,79 @@ export const RequestRoute = (api: API, options: RegisterOptions | undefined) => 
             }
         }
     );
+
+    api.post(Prefix + BaseURI,
+        Authenticated(),
+        async (request: Request, response: Response) => {
+            let authentication: Authentication = request.authentication;
+            
+            try {
+                
+                return response.status(201).json;
+            }
+            catch {
+                throw new NotFoundError('Request not created');
+            }
+        }
+    );
+
+    api.patch(Prefix + BaseURI + '/[id]',
+        ValidateMiddleware('params', { 'id': 'string' }),
+        Authenticated(),
+        async (request: Request, response: Response) => {
+            let authentication: Authentication = request.authentication;
+            
+            try {
+                return response.status(204).json;
+            }
+            catch {
+                throw new NotFoundError('Request not updated');
+            }
+        }
+    );
+
+    api.post(Prefix + BaseURI + 'deactivate/[id]',
+        ValidateMiddleware('params', { 'id': 'string' }),
+        Authenticated(),
+        async (request: Request, response: Response) => {
+            let authentication: Authentication = request.authentication;
+
+            try {
+                return response.status(204).json;
+            }
+            catch {
+                throw new NotFoundError('Request not deactivated');
+            }
+        }
+    );
+
+    api.delete(Prefix + BaseURI + '/[id]',
+        ValidateMiddleware('params', { 'id': 'string'}),
+        Authenticated(),
+        async (request: Request, response: Response) => {
+            let authentication: Authentication = request.authencation;
+
+            try {
+                return response.status(204).json;
+            }
+            catch {
+                throw new NotFoundError('Request not deleted');
+            }
+        }
+    );
+
+    api.post(Prefix + BaseURI + '/review/[id]',
+        ValidateMiddleware('params', { 'id': 'string'}),
+        Authenticated(),
+        async (request: Request, response: Response) => {
+            let authentication: Authentication = request.authencation;
+
+            try {
+                return response.status(204).json;
+            }
+            catch {
+                throw new NotFoundError('Request not reviewed');
+            }
+        }
+    );
 }
