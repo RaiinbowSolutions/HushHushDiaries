729a193ed6d74a0af318d310c5766bc6a1807290
Dennis Thiesen <dennis.thiesen.1990@gmail.com>
2023-08-27 23:30:08 +0200

    Fixed Validation

--- api/src/utilities/validation.ts
+++ api/src/utilities/validation.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-const EmailValidationRegex = process.env.EMAIL_REGEX ? RegExp(process.env.EMAIL_REGEX) : /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const EmailValidationRegex = process.env.EMAIL_REGEX ? RegExp(process.env.EMAIL_REGEX) : /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 function validateEmail(email: string): boolean {
     return EmailValidationRegex.test(email);
