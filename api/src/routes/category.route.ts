516f38a1bd58216359d0f51d0b4cf8f8d49137fd
Christine Wiberg <chrissywhiskerz@outlook.com>
2023-08-28 01:36:04 +0200

    Updated category endpoints

--- api/src/routes/category.route.ts
+++ api/src/routes/category.route.ts
@@ -11,14 +11,21 @@ export const CategoryRoute = (api: API, options: RegisterOptions | undefined) =>
     const Prefix = options?.prefix || '';
     const BaseURI = '/categories';
 
+    /**
+     * @alias CategoryRoute_GetCounts
+     */
     api.get(Prefix + BaseURI + '/counts',
         Authenticated(), 
         async (request: Request, response: Response) => {
             let counts = await CategoryService.counts();
+
             return response.status(200).json({type: 'category', counts});
         }
     );
-
+    
+    /**
+     * @alias CategoryRoute_GetCategories
+     */
     api.get(Prefix + BaseURI,
         ValidateMiddleware('query', {
             'page': { type: 'number', required: false },
@@ -28,19 +35,18 @@ export const CategoryRoute = (api: API, options: RegisterOptions | undefined) =>
         async (request: Request, response: Response) => {
             let authentication: Authentication = request.authentication;
             let {limit, offset} = Pagination.getData(request);
+            let total = await CategoryService.counts();
+            let categories = await CategoryService.selects(offset, limit);
+            let filtered = await CategoryService.filters.categories(authentication.id, categories);
+            let pagination = Pagination.create(request, filtered, total);
 
-            try {
-                let total = await CategoryService.counts();
-                let categories = await CategoryService.selects(offset, limit);
-                let filtered = await CategoryService.filters.categories(authentication.id, categories);
-    
-                let pagination = Pagination.create(request, filtered, total);
-    
-                return response.status(200).json(pagination);
-            } catch (error) {}
+            return response.status(200).json(pagination);
         }
     );
 
+    /**
+     * @alias CategoryRoute_GetCategory
+     */
     api.get(Prefix + BaseURI + '/[id]',
         ValidateMiddleware('params', { 'id': 'string' }),
         Authenticated(),
@@ -49,87 +55,98 @@ export const CategoryRoute = (api: API, options: RegisterOptions | undefined) =>
 
             if (!Minify.validate(request.params.id as string)) throw new NotFoundError('Category not found');
             let id = Minify.decode(request.params.id as string);
+            let category = await CategoryService.select(id);
+            let filtered = await CategoryService.filters.category(authentication.id, category);
 
-            try {
-                let category = await CategoryService.select(id);
-                let filtered = await CategoryService.filters.category(authentication.id, category);
-
-                return response.status(200).json(filtered);
-            } catch (error) {
-                throw new NotFoundError('Category not found');
-            }
+            return response.status(200).json(filtered);
         }
     );
 
-    api.post(Prefix + BaseURI, 
+    /**
+     * @alias CategoryRoute_CreateCategory
+     */
+    api.post(Prefix + BaseURI,
+        ValidateMiddleware('body', {
+            'name': 'string',
+        }),
         Authenticated(),
-        async (request: Request, response: Response) => {
-            let authentication: Authentication = request.authentication;
+        async(request: Request, response: Response) => {
+            let name = request.body.name as string; 
+            let description = request.body.description as string;
+            let result = await CategoryService.insert(name, description);
+            let id = Minify.encode(result.insertId as bigint);
 
-            try {
-
-                return response.status(201).json;
-            }
-            catch {
-                throw new NotFoundError('Category not created');
-            }
+            return response.status(201).json({
+                created: true, 
+                id, 
+                path: `${request.path}/${id}`,
+            });
         }
-    ); 
+    );
 
+    /**
+     * @alias CategoryRoute_UpdateCategory
+     */
     api.patch(Prefix + BaseURI + '/[id]',
-        ValidateMiddleware('params', { 'id': 'string' }),
+        ValidateMiddleware('params', {id: 'string'}),
+        ValidateMiddleware('body', {
+            'name': 'string'
+        }),
         Authenticated(),
-        async (request: Request, response: Response) => {
-            let authentication: Authentication = request.authentication; 
-
+        async(request: Request, response: Response) => {
             if (!Minify.validate(request.params.id as string)) throw new NotFoundError('Category not found');
+
             let id = Minify.decode(request.params.id as string);
+            let name = request.body.name as string;
+            let description = request.body.description as string;
 
-            try {
+            let result = await CategoryService.update(id, {
+                name,
+                description,
+            });
 
-                return response.status(204).json;
-            }
-            catch {
-                throw new NotFoundError('Category not updated');
-            }
+            return response.status(200).json({
+                updated: true,
+                updated_rows: '' + result.numUpdatedRows,
+            });
         }
     );
 
-    api.post(Prefix + BaseURI + '/deactivate/[id',
-        ValidateMiddleware('params', { 'id': 'string' }),
+    /**
+     * @alias CategoryRoute_DeactivateCategory
+     */
+    api.post(Prefix + BaseURI + '/deactivate/[id]',
+        ValidateMiddleware('params', {id: 'string'}),
         Authenticated(),
-        async (request: Request, response: Response) => 
-        {
-            let authentication: Authentication = request.authentication;
-
+        async(request: Request, response: Response) => {
             if (!Minify.validate(request.params.id as string)) throw new NotFoundError('Category not found');
+
             let id = Minify.decode(request.params.id as string);
+            let result = await CategoryService.markAsDeleted(id);
 
-            try {
-
-                return response.status(204).json;
-            }
-            catch {
-                throw new NotFoundError('Category not deactivated');
-            }
+            return response.status(204).json({
+                deactivated: true, 
+                deactivated_rows: result.numUpdatedRows,
+            });
         }
     );
 
-    api.delete(Prefix + BaseURI + '/[id]', 
-        ValidateMiddleware('params', { 'id': 'string' }),
+    /**
+     * @alias CategoryRoute_DeleteCategory
+     */
+    api.delete(Prefix + BaseURI + '/[id]',
+        ValidateMiddleware('params', {id: 'string'}),
         Authenticated(),
-        async (request: Request, response: Response) => {
-            let authentication: Authentication = request.authentication;
-
+        async(request: Request, response: Response) => {
             if (!Minify.validate(request.params.id as string)) throw new NotFoundError('Category not found');
-            let id = Minify.decode(request.params.id as string);
 
-            try {
-                return response.status(204).json;
-            }
-            catch {
-                throw new NotFoundError('Category not deleted');
-            }
+            let id = Minify.decode(request.params.id as string);
+            let result = await CategoryService.delete(id);
+
+            return response.status(204).json({
+                deleted: true, 
+                deleted_rows: result.numDeletedRows,
+            });
         }
     );
 }
