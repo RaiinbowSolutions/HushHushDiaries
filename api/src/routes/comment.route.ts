29802e0e6bdf40715758a09b8632415effe61819
Christine Benedicte Wiberg <christine@jo-informatik.dk>
2023-08-28 10:08:54 +0200

    Updated CommentRoute

--- api/src/routes/comment.route.ts
+++ api/src/routes/comment.route.ts
@@ -66,5 +66,171 @@ export const CommentRoute = (api: API, options: RegisterOptions | undefined) => 
     /**
      * @alias CommentRoute_CreateComment
      */
-    
+    api.post(Prefix + BaseURI, 
+        ValidateMiddleware('body', {
+            'content': 'string'
+        }),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            let content = request.body.content as string;
+            let id = Minify.encode('comments', request.insertId as bigint);
+
+            return response.status(201).json({
+                created: true, 
+                id, 
+                path: `${request.path}/${id}`,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_UpdateComment
+     */
+    api.patch(Prefix + BaseURI + '/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+            let content = request.body.content as string;
+
+            let result = await CommentService.update(id, {
+                content,
+            });
+
+            return response.status(200).json({
+                updated: true, 
+                updated_rows: result.numUpdatedRows,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_DeactivateComment
+     */
+    api.post(Prefix + BaseURI + '/deactivate/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+            let result = await CommentService.markAsDeleted(id);
+
+            return response.status(204).json({
+                deactivated: true, 
+                deactivated_rows: result.numUpdatedRows,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_DeleteComment
+     */
+    api.delete(Prefix + BaseURI + '/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+            
+            let id = Minify.decode('comments', request.params.id as string);
+            let result = await CommentService.delete(id);
+
+            return response.status(204).json({
+                deleted: true, 
+                deleted_rows: result.numDeletedRows,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_ApproveComment
+     */
+    api.post(Prefix + BaseURI + '/approve/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+            let result = await CommentService.markAsApproved(id);
+
+            return response.status(204).json({
+                approved: true, 
+                approved_rows: result.numUpdatedRows,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_ReviewComment
+     */
+    api.post(Prefix + BaseURI + '/review/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+            let result = await CommentService.markAsReviewed(id);
+
+            return response.status(204).json({
+                reviewed: true, 
+                reviewed_rows: result.numUpdatedRows,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_CommentLikeCount
+     */
+    api.get(Prefix + BaseURI + '/likes/count/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+            let countLikes = await CommentService.countLikes(id);
+
+            return response.status(200).json({
+                countLikes: true,
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_LikeOnComment
+     */
+    api.post(Prefix + BaseURI + '/likes/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+
+            return response.status(204).json({
+                commentLiked: true, 
+            });
+        }
+    );
+
+    /**
+     * @alias CommentRoute_UnlikeOnComment
+     */
+    api.delete(Prefix + BaseURI + '/likes/:[id]',
+        ValidateMiddleware('params', {id: 'string'}),
+        Authenticated(),
+        async(request: Request, response: Response) => {
+            if (!Minify.validate('comments', request.params.id as string)) throw new NotFoundError('Comment not found');
+
+            let id = Minify.decode('comments', request.params.id as string);
+
+            return response.status(204).json({
+                commentUnliked: true, 
+            });
+        }
+    );
 }
