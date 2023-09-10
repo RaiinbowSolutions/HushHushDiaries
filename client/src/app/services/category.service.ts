b0e294649ea0273d5ed6d08f3a0ec0b54e06988d
Christine Wiberg <chrissywhiskerz@outlook.com>
2023-09-10 15:53:00 +0200

    Changes to categoryservice

--- client/src/app/services/category.service.ts
+++ client/src/app/services/category.service.ts
@@ -17,8 +17,10 @@ export class CategoryService {
     }
     
     categoriesFetchFailed = new EventEmitter(true);
+    categoryFetchFailed = new EventEmitter(true);
 
     categoriesFetched = new EventEmitter(true);
+    categoryFetch = new EventEmitter(true);
 
     constructor(
         private http: HttpClient,
@@ -26,6 +28,21 @@ export class CategoryService {
 
     getCategories() {
         let result = this.http.get<Pagination<Category>>(this.path, this.options);
+        result.subscribe({
+            next: () => this.categoriesFetched.emit(true), 
+            error: () => this.categoriesFetchFailed.emit(true),
+        });
+
+        return result;
+    }
+
+    getCategory(id: string) {
+        let result = this.http.get<Category>(this.path + `/${id}}`, this.options);
+        
+        result.subscribe({
+            next: () => this.categoryFetch.emit(true),
+            error: () => this.categoryFetchFailed.emit(true),
+        });
 
         return result;
     }
