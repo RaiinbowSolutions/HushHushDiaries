ba2bd254c98ed6b0bb3c8281415398bbe96eef8b
Christine Wiberg <chrissywhiskerz@outlook.com>
2023-09-10 21:23:26 +0200

    Updated some frontend componetns and pages

--- client/src/app/pages/blogs/blog-create/blog-create.page.ts
+++ client/src/app/pages/blogs/blog-create/blog-create.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-blog-create',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-create.page.css']
 })
 export class BlogCreatePage {
+  form: FormGroup;
 
+  title = new FormControl('');
+  keywords = new FormControl('');
+  description = new FormControl('');
+  content = new FormControl('');
+  categoryid = new FormControl('');
+  authorid = new FormControl('');
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private blogService: BlogService,
+    private http: HttpClient,
+  ) { 
+    this.form = this.formBuilder.group({
+      title: this.title,
+      keywords: this.keywords,
+      description: this.description,
+      content: this.content,
+      categoryid: this.categoryid,
+      authorid: this.authorid,
+    })
+  }
+
+  create() {
+    const data = this.form.value; 
+    if (data.title && data.keywords && data.description && data.content && data.categoryid && data.authorid) {
+      this.blogService.create(data);
+    }
+  }
 }
